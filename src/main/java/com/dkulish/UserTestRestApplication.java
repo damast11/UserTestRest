66828441d161382a822111402e91e9cfeeb8bcbc
Dmytro Kulish <Dmytro_Kulish@epam.com>
2023-03-20 09:39:30 +0200

    init commit

---
+++ src/main/java/com/dkulish/UserTestRestApplication.java
@@ -0,0 +1,13 @@
+package com.dkulish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTestRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserTestRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dkulish/UserTestRestApplicationTests.java
@@ -0,0 +1,8 @@
+package com.dkulish;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTestRestApplicationTests {
+
+}
